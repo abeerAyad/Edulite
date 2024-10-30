018860b28c5cf83c53445637b810353ea2ed6e02
abeerAyad <abeerayyad2000@gmail.com>
2024-10-30 23:19:57 +0200

    file structure and download tailwind animatecss and owl

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/pages/home/index.html',
+    './public/pages/signup/index.html' ,
+    './public/pages/courses/index.html' ,
+    './public/pages/login/index.html',
+    './public/pages/pricing/index.html',
+    './public/pages/course-details/index.html',
+    './public/pages/about/index.html'
+  ],
+  theme: {
+    extend: {
+     colors: {
+      primary:"#FF9500",
+      "text-color":"#262626",
+      secondary:"#FFD599"
+     },
+     fontFamily: {
+       "vietnam":'"Be Vietnam Pro", sans-serif',
+     }
+    },
+  },
+  plugins: [],
+}
+
+
